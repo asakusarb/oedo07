e1fea6a76b7237f670f42fa29bfd0bbf55235151
llminatoll <cam51p.g@gmail.com>
2018-06-25 22:05:47 +0900

    make branch 'gh-pages' for Travis CI

--- src/js/app.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-import $ from 'jquery';
-import '../../node_modules/sass-basis/src/js/basis.js';
-import BasisFixedHeader from '../../node_modules/sass-basis-layout/src/js/fixed-header.js';
-import BasisStickyHeader from '../../node_modules/sass-basis-layout/src/js/sticky-header.js';
-
-new BasisFixedHeader();
-new BasisStickyHeader();
